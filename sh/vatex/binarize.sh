c8e9106baa7785b269b78425657c737a7db4ca35
kly <1151712307@qq.com>
2022-06-06 01:25:02 +0800

    2

--- sh/vatex/binarize.sh
+++ sh/vatex/binarize.sh
@@ -3,7 +3,7 @@ tgt='zh'
 TEXT='/home/sata/kly/videoNMT/data/raw_texts'
 fairseq-preprocess --source-lang $src --target-lang $tgt \
   --trainpref $TEXT/train.tok.bpe \
-  --validpref $TEXT/valid.tok.bpe \
+  --validpref $TEXT/dev.tok.bpe \
   --testpref $TEXT/test.tok.bpe \
   --destdir $TEXT/data-bin/src_tgt \
   --workers 8
