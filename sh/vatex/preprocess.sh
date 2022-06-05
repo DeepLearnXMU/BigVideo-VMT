c7424eda6cc22e24a6aecf12623cd6ee5e3f0d90
kly <1151712307@qq.com>
2022-06-06 01:16:03 +0800

    2

--- sh/vatex/preprocess.sh
+++ sh/vatex/preprocess.sh
@@ -4,15 +4,16 @@ DATA="/home/sata/kly/videoNMT/data/raw_texts"
 script_root=/home/kly/fairseq/perl
 tokenizer=$script_root/tokenizer.perl
 
+#
 #tokenization
-echo "-----tokenization-------"
-perl $tokenizer -l en <$DATA/train.en> $DATA/train.tok.en
-perl $tokenizer -l en <$DATA/dev.en> $DATA/dev.tok.en
-perl $tokenizer -l en <$DATA/test.en> $DATA/test.tok.en
+#echo "-----tokenization-------"
+#perl $tokenizer -l en <$DATA/train.en> $DATA/train.tok.en
+#perl $tokenizer -l en <$DATA/dev.en> $DATA/dev.tok.en
+#perl $tokenizer -l en <$DATA/test.en> $DATA/test.tok.en
 
   ##learn BPE
 BPE_ROOT=/home/kly/subword-nmt
-python $BPE_ROOT/learn_bpe.py    \
+python $BPE_ROOT/learn_joint_bpe_and_vocab.py    \
        --input $DATA/train.tok.en  -s 10000 -o $DATA/bpe.10000 \
        --write-vocabulary $DATA/vocab.10000.en
 #apply bpe
