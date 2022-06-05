9bf60d46c7168970a60680622d1aa3ae1e0ed8e7
kly <1151712307@qq.com>
2022-06-05 21:23:02 +0800

    2

--- sh/extract_img_features.sh
+++ sh/extract_img_features.sh
@@ -1,10 +1,10 @@
-python3 scripts/get_img_feat.py --dataset train --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+#python3 scripts/get_img_feat.py --dataset train --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
-python3 scripts/get_img_feat.py --dataset val --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+python3 scripts/get_img_feat.py --dataset vit_large_patch16_384 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
-python3 scripts/get_img_feat.py --dataset test2016 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
-
-python3 scripts/get_img_feat.py --dataset test2017 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+#python3 scripts/get_img_feat.py --dataset test2016 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+#
+#python3 scripts/get_img_feat.py --dataset test2017 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
 
 
