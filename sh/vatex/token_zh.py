9984c89a20cbb95faaf944dee3f1903f684ac62c
kly <1151712307@qq.com>
2022-06-06 00:56:27 +0800

    2

--- sh/vatex/token_zh.py
+++ sh/vatex/token_zh.py
@@ -3,6 +3,7 @@ from tqdm import tqdm,trange
 
 train='/home/sata/kly/videoNMT/data/raw_texts/train.zh'
 dev='/home/sata/kly/videoNMT/data/raw_texts/dev.zh'
+output='/home/sata/kly/videoNMT/data/raw_texts/'
 nlp = spacy.load("zh_core_web_sm")
 
 with open(train,encoding='utf-8') as file:
@@ -19,6 +20,13 @@ for i in trange(len(train)):
 for i in trange(len(dev)):
     dev[i]=" ".join([t.text for t in  nlp(dev[i])])
 
+
 print(train[:3])
 print(dev[:3])
 
+with open(output+"train.tok.zh", "w", encoding="utf-8") as w:
+    for line in tqdm(train):
+        w.write(line+ "\n")
+with open(output+"dev.tok.zh", "w", encoding="utf-8") as w:
+    for line in tqdm(dev):
+        w.write(line+ "\n")
