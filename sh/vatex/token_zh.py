b1f280f77fe8d6ddf1e5fa709fe01311d58b5db7
kly <1151712307@qq.com>
2022-06-06 00:53:19 +0800

    2

--- sh/vatex/token_zh.py
+++ sh/vatex/token_zh.py
@@ -1,5 +1,21 @@
 import spacy
+train='/home/sata/kly/videoNMT/data/raw_texts/train.zh'
+dev='/home/sata/kly/videoNMT/data/raw_texts/dev.zh'
 nlp = spacy.load("zh_core_web_sm")
-doc = nlp("庆祝祖国生日快乐,哦耶")
-result=" ".join([t.text for t in doc])
-print(result)
+
+with open(train,encoding='utf-8') as file:
+    train=[x.rstrip() for x in file.readlines()]
+
+with open(dev, encoding='utf-8') as file:
+    dev = [x.rstrip() for x in file.readlines()]
+print(train[:3])
+print(dev[:3])
+
+for i in range(len(train)):
+    train[i]=" ".join([t.text for t in  nlp(train[i])])
+
+for i in range(len(dev)):
+    dev[i]=" ".join([t.text for t in  nlp(dev[i])])
+print(train[:3])
+print(dev[:3])
+
