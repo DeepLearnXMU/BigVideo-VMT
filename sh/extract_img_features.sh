77282a0eb72bae05fe3da52ead7982c6a155f2ce
kly <1151712307@qq.com>
2022-06-05 21:43:38 +0800

    2

--- sh/extract_img_features.sh
+++ sh/extract_img_features.sh
@@ -1,5 +1,5 @@
 
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=4,5
 #python3 scripts/get_img_feat.py --dataset train --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
 python3 scripts/get_img_feat.py --dataset val --model vit_large_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
