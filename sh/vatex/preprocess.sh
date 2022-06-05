8eaf42252afab179ef9d7274fa52a1c001d4e8cc
kly <1151712307@qq.com>
2022-06-06 01:08:16 +0800

    2

--- sh/vatex/preprocess.sh
+++ sh/vatex/preprocess.sh
@@ -1,12 +1,22 @@
 
-DATA="/home/sata/kly/videoNMT/data/raw_texts/"
+DATA="/home/sata/kly/videoNMT/data/raw_texts"
 
 script_root=/home/kly/fairseq/perl
 tokenizer=$script_root/tokenizer.perl
 
 #tokenization
 echo "-----tokenization-------"
-perl $tokenizer -l en <$DATA/train.norm.en> $DATA/train.norm.tok.en
+perl $tokenizer -l en <$DATA/train.en> $DATA/train.tok.en
+perl $tokenizer -l en <$DATA/dev.en> $DATA/dev.tok.en
+perl $tokenizer -l en <$DATA/test.en> $DATA/test.tok.en
 
-
+  ##learn BPE
+BPE_ROOT=/home/kly/subword-nmt
+python $BPE_ROOT/learn_bpe.py    \
+       --input $DATA/train.tok.en  -s 10000 -o $DATA/bpe.10000 \
+       --write-vocabulary $DATA/vocab.10000.en
+#apply bpe
+python $BPE_ROOT/apply_bpe.py -c $DATA/bpe.10000 < $DATA/train.tok.en > $DATA/train.tok.bpe.en
+python $BPE_ROOT/apply_bpe.py -c $DATA/bpe.10000 < $DATA/dev.tok.en > $DATA/dev.tok.bpe.en
+python $BPE_ROOT/apply_bpe.py -c $DATA/bpe.10000 < $DATA/test.tok.en > $DATA/test.tok.bpe.en
 
