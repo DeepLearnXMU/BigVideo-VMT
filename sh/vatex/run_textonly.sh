9eaa7571894ba0f6a7ef5cd3b68c5725f00450cf
kly <1151712307@qq.com>
2022-06-06 10:26:04 +0800

    2

--- sh/vatex/run_textonly.sh
+++ sh/vatex/run_textonly.sh
@@ -24,6 +24,7 @@ weight_decay=0.1
 arch=transformer_vatex
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
+
 name=baseline_arch${arch}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_mt${max_tokens}_wd${weight_decay}_patience${patience}
 
 output_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/${name}
