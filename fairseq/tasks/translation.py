0d804d8ea223c6005eeed5902974fe27503eced0
kly <1151712307@qq.com>
2022-06-06 16:08:15 +0800

    2

--- fairseq/tasks/translation.py
+++ fairseq/tasks/translation.py
@@ -434,10 +434,10 @@ class TranslationTask(LegacyFairseqTask):
         gen_out = self.inference_step(generator, [model], sample, prefix_tokens=None)
         hyps, refs = [], []
         for i in range(len(gen_out)):
-            print(gen_out[i][0]["tokens"])
-            print(utils.strip_pad(sample["target"][i], self.tgt_dict.pad()))
-            print(sacrebleu.corpus_bleu(gen_out[i][0]["tokens"],utils.strip_pad(sample["target"][i], self.tgt_dict.pad())))
-            print(sda)
+            # print(gen_out[i][0]["tokens"])
+            # print(utils.strip_pad(sample["target"][i], self.tgt_dict.pad()))
+            # print(sacrebleu.corpus_bleu(gen_out[i][0]["tokens"],utils.strip_pad(sample["target"][i], self.tgt_dict.pad())))
+            # print(sda)
             hyps.append(decode(gen_out[i][0]["tokens"]))
             refs.append(
                 decode(
@@ -447,7 +447,7 @@ class TranslationTask(LegacyFairseqTask):
             )
         print(hyps[0])
         print(refs[0])
-        print(sacrebleu.corpus_bleu(hyps[0],refs[0]))
+        print(sacrebleu.corpus_bleu([hyps[0]],[[refs[0]]]))
         print(dsada)
         if self.args.eval_bleu_print_samples:
             logger.info("example hypothesis: " + hyps[0])
