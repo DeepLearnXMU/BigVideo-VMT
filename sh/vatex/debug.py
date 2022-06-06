415f797dabd79c41d8c526978cf986564bfb2843
kly <1151712307@qq.com>
2022-06-06 16:51:58 +0800

    2

--- sh/vatex/debug.py
+++ sh/vatex/debug.py
@@ -7,5 +7,5 @@ hypos=['三名潜水员正在水下使用一台机器。', '两个人在外面�
 refs=['三个潜水员在水下共同使用一台机器。', '两个人从外面不同的树上摘新鲜水果。', '一个小女孩在路上不停的蹬着滑板车。']
 
 
-print(sacrebleu.corpus_bleu(hypos, [refs]))
-print(corpus_bleu(refs, hypos))
+print(sacrebleu.corpus_bleu(hypos, [hypos]))
+print(corpus_bleu(hypos, hypos))
