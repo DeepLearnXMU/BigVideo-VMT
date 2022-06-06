faed1b7625c966f5e065e1b77641a03b8bc62765
kly <1151712307@qq.com>
2022-06-06 17:24:32 +0800

    2

--- sh/vatex/debug.py
+++ sh/vatex/debug.py
@@ -5,7 +5,7 @@ import inspect
 
 fn_sig = inspect.getfullargspec(sacrebleu)
 print(fn_sig)
-bleu = sacrebleu.BLEU(trg_lang='zh', tokenize='zh')
+bleu = sacrebleu.BLEU(tokenize='zh')
 
 hypos=['三名潜水员正在水下使用一台机器。', '两个人在外面从不同的树上采摘新鲜的水果。', '一个年轻的女孩在附近的附近骑着滑板车。']
 refs=['三个潜水员在水下共同使用一台机器。', '两个人从外面不同的树上摘新鲜水果。', '一个小女孩在路上不停的蹬着滑板车。']
