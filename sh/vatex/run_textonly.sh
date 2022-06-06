dfa4c9f7b5dc7077a3fc4770a9c2fe409075a122
kly <1151712307@qq.com>
2022-06-07 00:24:22 +0800

    2

--- sh/vatex/run_textonly.sh
+++ sh/vatex/run_textonly.sh
@@ -21,7 +21,7 @@ patience=10
 max_epoches=100
 dropout=0.3
 seed=42
-weight_decay=0.01
+weight_decay=0.0
 arch=transformer_tiny
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
