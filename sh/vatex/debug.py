82e564e68a2f93f455c416dcd2b020e90149d9e6
kly <1151712307@qq.com>
2022-06-06 16:52:52 +0800

    2

--- sh/vatex/debug.py
+++ sh/vatex/debug.py
@@ -6,6 +6,6 @@ from nltk.translate.bleu_score import corpus_bleu
 hypos=['三名潜水员正在水下使用一台机器。', '两个人在外面从不同的树上采摘新鲜的水果。', '一个年轻的女孩在附近的附近骑着滑板车。']
 refs=['三个潜水员在水下共同使用一台机器。', '两个人从外面不同的树上摘新鲜水果。', '一个小女孩在路上不停的蹬着滑板车。']
 
-
+hypos=['A man is here.', 'A girl is here.', 'A woman is here.']
 print(sacrebleu.corpus_bleu(hypos, [hypos]))
 print(corpus_bleu(hypos, hypos))
