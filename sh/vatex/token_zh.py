3fcd581c2b17ddd6c8806eef581ea6453598557e
kly <1151712307@qq.com>
2022-06-06 15:08:09 +0800

    2

--- sh/vatex/token_zh.py
+++ sh/vatex/token_zh.py
@@ -7,26 +7,26 @@ output='/home/sata/kly/videoNMT/data/raw_texts/'
 nlp = spacy.load("zh_core_web_sm")
 
 with open(train,encoding='utf-8') as file:
-    train=[x.rstrip() for x in file.readlines()]
+    train=[" ".join(x.rstrip()) for x in file.readlines()]
 
 with open(dev, encoding='utf-8') as file:
-    dev = [x.rstrip() for x in file.readlines()]
+    dev = [" ".join(x.rstrip()) for x in file.readlines()]
 print(train[:3])
 print(dev[:3])
 
-for i in trange(len(train)):
-    train[i]=" ".join([t.text for t in  nlp(train[i])])
-
-for i in trange(len(dev)):
-    dev[i]=" ".join([t.text for t in  nlp(dev[i])])
+# for i in trange(len(train)):
+#     train[i]=" ".join([t.text for t in  nlp(train[i])])
+#
+# for i in trange(len(dev)):
+#     dev[i]=" ".join([t.text for t in  nlp(dev[i])])
 
 
 print(train[:3])
 print(dev[:3])
 
-with open(output+"train.tok.zh", "w", encoding="utf-8") as w:
+with open(output+"train.tok-char.zh", "w", encoding="utf-8") as w:
     for line in tqdm(train):
         w.write(line+ "\n")
-with open(output+"dev.tok.zh", "w", encoding="utf-8") as w:
+with open(output+"dev.tok-char.zh", "w", encoding="utf-8") as w:
     for line in tqdm(dev):
         w.write(line+ "\n")
