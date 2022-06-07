802f43339347dc69d7a93cfe749bdfb792f77152
kly <1151712307@qq.com>
2022-06-07 15:50:25 +0800

    2

--- sh/vatex/test_valid.sh
+++ sh/vatex/test_valid.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=5
 
 
-checkpoint_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/textonly_char_archtransformer_tiny_tgtzh_lr0.003_wu2000_me100_seed1_gpu1_mt4096_wd0.1_patience10/
+checkpoint_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/textonly_char_archtransformer_tiny_tgtzh_lr0.002_wu2000_me100_seed1_gpu1_mt4096_wd0.1_patience10/
 
 script_root=/home/kly/fairseq/perl
 multi_bleu=$script_root/multi-bleu.perl
@@ -14,15 +14,15 @@ ensemble=10
 
 checkpoint=checkpoint_best.pt
 
-echo "-----$who-------"
-fairseq-generate  $test_DATA  \
---path $checkpoint_dir/$checkpoint \
---remove-bpe \
---gen-subset $who \
---beam 5  \
---batch-size 128  \
---lenpen 0.8   \
---output $checkpoint_dir/$checkpoint.gen-$who.txt  | tee $checkpoint_dir/$checkpoint.gen-$who.log
+#echo "-----$who-------"
+#fairseq-generate  $test_DATA  \
+#--path $checkpoint_dir/$checkpoint \
+#--remove-bpe \
+#--gen-subset $who \
+#--beam 5  \
+#--batch-size 128  \
+#--lenpen 0.8   \
+#--output $checkpoint_dir/$checkpoint.gen-$who.txt  | tee $checkpoint_dir/$checkpoint.gen-$who.log
 
 
 if [ -n "$ensemble" ]; then
