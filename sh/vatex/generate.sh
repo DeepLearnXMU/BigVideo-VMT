6115ba2c913323646d60974134605506ed340b98
kly <1151712307@qq.com>
2022-06-07 10:27:46 +0800

    2

--- sh/vatex/generate.sh
+++ sh/vatex/generate.sh
@@ -13,17 +13,17 @@ ensemble=10
 
 checkpoint=checkpoint_best.pt
 
-echo "-----$who-------"
-fairseq-generate  $test_DATA  \
---path $checkpoint_dir/$checkpoint \
---remove-bpe \
---gen-subset $who \
---beam 5  \
---batch-size 128  \
---lenpen 0.8   \
---output $checkpoint_dir/$checkpoint.gen-$who.txt
-
-python3 rerank.py $checkpoint_dir/$checkpoint.gen-$who.txt $checkpoint_dir/$checkpoint.gen-$who.txt.sorted
+#echo "-----$who-------"
+#fairseq-generate  $test_DATA  \
+#--path $checkpoint_dir/$checkpoint \
+#--remove-bpe \
+#--gen-subset $who \
+#--beam 5  \
+#--batch-size 128  \
+#--lenpen 0.8   \
+#--output $checkpoint_dir/$checkpoint.gen-$who.txt
+#
+#python3 rerank.py $checkpoint_dir/$checkpoint.gen-$who.txt $checkpoint_dir/$checkpoint.gen-$who.txt.sorted
 
 echo "-----formating json-----"
 ids_dir="/home/sata/kly/videoNMT/data/raw_texts/test.ids"
@@ -34,28 +34,28 @@ python3 sh/vatex/construct_json.py $ids_dir $hypos_dir $result_path $checkpoint
 echo "-----done-----"
 
 
-if [ -n "$ensemble" ]; then
-      if [ ! -e "$checkpoint_dir/last$ensemble.ensemble.pt" ]; then
-              PYTHONPATH=`pwd` python3 scripts/average_checkpoints.py --inputs $checkpoint_dir --output $checkpoint_dir/last$ensemble.ensemble.pt --num-epoch-checkpoints $ensemble
-      fi
-      checkpoint=last$ensemble.ensemble.pt
-fi
+#if [ -n "$ensemble" ]; then
+#      if [ ! -e "$checkpoint_dir/last$ensemble.ensemble.pt" ]; then
+#              PYTHONPATH=`pwd` python3 scripts/average_checkpoints.py --inputs $checkpoint_dir --output $checkpoint_dir/last$ensemble.ensemble.pt --num-epoch-checkpoints $ensemble
+#      fi
+#      checkpoint=last$ensemble.ensemble.pt
+#fi
+#
+#
+#
+#echo "-----$who ensemble-------"
+#fairseq-generate  $test_DATA  \
+#--path $checkpoint_dir/$checkpoint \
+#--remove-bpe \
+#--gen-subset $who \
+#--beam 5  \
+#--batch-size 128  \
+#--lenpen 0.8   \
+#--output $checkpoint_dir/$checkpoint.gen-$who.txt
+#
+#python3 rerank.py $checkpoint_dir/$checkpoint.gen-$who.txt $checkpoint_dir/$checkpoint.gen-$who.txt.sorted
 
-
-
-echo "-----$who-------"
-fairseq-generate  $test_DATA  \
---path $checkpoint_dir/$checkpoint \
---remove-bpe \
---gen-subset $who \
---beam 5  \
---batch-size 128  \
---lenpen 0.8   \
---output $checkpoint_dir/$checkpoint.gen-$who.txt
-
-python3 rerank.py $checkpoint_dir/$checkpoint.gen-$who.txt $checkpoint_dir/$checkpoint.gen-$who.txt.sorted
-
-echo "-----formating json-----"
+echo "-----formating json ensemble-----"
 ids_dir="/home/sata/kly/videoNMT/data/raw_texts/test.ids"
 hypos_dir=$checkpoint_dir/$checkpoint.gen-$who.txt.sorted
 result_path=$checkpoint_dir
