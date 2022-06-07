9c02299b7b3efb634376c53a6d03326a7bc35f6f
kly <1151712307@qq.com>
2022-06-07 15:46:44 +0800

    2

--- sh/vatex/test_valid.sh
+++ sh/vatex/test_valid.sh
@@ -3,7 +3,7 @@
 export CUDA_VISIBLE_DEVICES=5
 
 
-checkpoint_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/textonly_char_archtransformer_tiny_tgtzh_lr0.004_wu2000_me100_seed1_gpu1_mt4096_wd0.1_patience10/
+checkpoint_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/textonly_char_archtransformer_tiny_tgtzh_lr0.003_wu2000_me100_seed1_gpu1_mt4096_wd0.1_patience10/
 
 script_root=/home/kly/fairseq/perl
 multi_bleu=$script_root/multi-bleu.perl
