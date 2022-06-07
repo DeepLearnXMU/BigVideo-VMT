802806f15348255ebbed064aad5a6d8cc5a0a481
kly <1151712307@qq.com>
2022-06-07 14:37:17 +0800

    2

--- sh/extract_img_features.sh
+++ sh/extract_img_features.sh
@@ -1,12 +1,14 @@
 
 export CUDA_VISIBLE_DEVICES=4,5
-#python3 scripts/get_img_feat.py --dataset train --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+python3 scripts/get_img_feat.py --dataset train --model vit_base_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
-python3 scripts/get_img_feat.py --dataset val --model vit_large_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+python3 scripts/get_img_feat.py --dataset val --model vit_base_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
-#python3 scripts/get_img_feat.py --dataset test2016 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
-#
-#python3 scripts/get_img_feat.py --dataset test2017 --model resnet50 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+python3 scripts/get_img_feat.py --dataset test2016 --model vit_base_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+
+python3 scripts/get_img_feat.py --dataset test2017 --model vit_base_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
+
+python3 scripts/get_img_feat.py --dataset testcoco --model vit_base_patch16_384 --path  /home/kly/fairseq_mmt-main/data/flickr30k
 
 
 
