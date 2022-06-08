0ff7f9ce111166dea702275e777445f7c5b5725b
kly <1151712307@qq.com>
2022-06-08 10:52:18 +0800

    2

--- sh/vatex/run_vatex_gated.sh
+++ sh/vatex/run_vatex_gated.sh
@@ -32,7 +32,7 @@ video_feat_dim=1024
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
 
-name=textonly_char_arch${arch}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_mt${max_tokens}_acc${update_freq}_wd${weight_decay}_patience${patience}
+name=vatex_textonly_char_arch${arch}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_mt${max_tokens}_acc${update_freq}_wd${weight_decay}_patience${patience}
 
 output_dir=/home/sata/kly/fairseq_mmt/output/vatex_baseline/${name}
 
