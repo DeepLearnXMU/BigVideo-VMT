c0a07071899223847fce8a7ce3e7e83f4a739e05
kly <1151712307@qq.com>
2022-06-14 21:48:50 +0800

    2

--- sh/vatex/run_vatex_sa.sh
+++ sh/vatex/run_vatex_sa.sh
@@ -25,7 +25,7 @@ dropout=0.3
 seed=1
 weight_decay=0.1
 clip_norm=0.0
-arch=vatex_multimodal_transformer_att_vatex_top_pe
+arch=vatex_multimodal_transformer_att_vatex_top_nope
 
 video_feat_path=/home/sata/kly/videoNMT/data/vatex_features
 video_ids_path=/home/sata/kly/videoNMT/data/raw_texts/ids
