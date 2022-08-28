9c3ada4675b3eb43d1768f857e354af18ef1aefd
kangliyan <kangliyan@bytedance.com>
2022-08-28 22:00:28 +0800

    1

--- fairseq/models/video/video_swin/swin_transformer.py
+++ fairseq/models/video/video_swin/swin_transformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 import numpy as np
-from fairseq.models.timm.models.layers import DropPath, trunc_normal_
+from timm.models.layers import DropPath, trunc_normal_
 
 # from mmcv.runner import load_checkpoint
 
