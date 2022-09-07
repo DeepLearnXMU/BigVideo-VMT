492e3d35d7ad3f0f76ff64820ae018d9e264d2c4
kangliyan <kangliyan@bytedance.com>
2022-09-07 19:17:28 +0800

    1

--- sh/xigua/mask/run_video_translation_withVIT.sh
+++ sh/xigua/mask/run_video_translation_withVIT.sh
@@ -66,11 +66,13 @@ freeze_backbone=True
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
 
-name=${mask}_arch${arch}_cri${cri}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_wd${weight_decay}_vtype${video_feat_type}_mf${max_num_frames}_rp${residual_policy}_ia${ini_alpha}_patience${patience}_fr{freeze_backbone}
+name=${mask}_arch${arch}_cri${cri}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_wd${weight_decay}_vtype${video_feat_type}_mf${max_num_frames}_rp${residual_policy}_ia${ini_alpha}_patience${patience}_fr${freeze_backbone}
 
-output_dir=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_output/xigua/${mask}/ve_clip_double_cross_att/${name}
-LOGS_DIR=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_logs/xigua/${mask}/ve_clip_double_cross_att
-local_logs_dir=~/fairseq_logs/xigua/${mask}/ve_clip_double_cross_att
+
+
+output_dir=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_output/xigua/${mask}/${name}
+LOGS_DIR=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_logs/xigua/${mask}/
+local_logs_dir=~/fairseq_logs/xigua/${mask}/
 
 hdfs dfs -mkdir -p $LOGS_DIR
 hdfs dfs -mkdir -p $output_dir
