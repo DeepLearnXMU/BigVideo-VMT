885a4c217d74375ad7d897e0d35f26f7c4537522
kangliyan <kangliyan@bytedance.com>
2022-09-14 15:44:28 +0800

    1

--- sh/xigua/mask/run_textonly.sh
+++ sh/xigua/mask/run_textonly.sh
@@ -34,7 +34,7 @@ clip_norm=0.0
 
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
-name=${mask}_arch${arch}_tgt${tgt_lang}_lr${lr}_wu${warmup}_seed${seed}_gpu${gpu_num}_mt${max_tokens}_acc${update_freq}_wd${weight_decay}_cn${clip_norm}_Realpatience${patience}
+name=${mask}ed20_arch${arch}_tgt${tgt_lang}_lr${lr}_wu${warmup}_seed${seed}_gpu${gpu_num}_mt${max_tokens}_acc${update_freq}_wd${weight_decay}_cn${clip_norm}_Realpatience${patience}
 
 output_dir=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_output/xigua/${mask}/${name}
 LOGS_DIR=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_logs/xigua/${mask}
