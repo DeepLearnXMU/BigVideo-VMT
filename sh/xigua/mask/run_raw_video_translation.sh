42da0c77bff3223f580c826a97e9b891de2c798c
kangliyan <kangliyan@bytedance.com>
2022-09-19 18:43:17 +0800

    1

--- sh/xigua/mask/run_raw_video_translation.sh
+++ sh/xigua/mask/run_raw_video_translation.sh
@@ -23,28 +23,28 @@ if [ $criterion == "label_smoothed_cross_entropy" ]; then
         cri=CMCCTR
 fi
 
-mask=$1    #mask1,2,3,4,c,p
-local_data_dir=/mnt/bd/kangliyan/data/vatex/fairseq_bin/fairseq_bin_filter/vatex.en-zh.${mask}
+mask=$1   #mask1,2,3,4,c,p
+seed=$2
+arch=$3
+video_feat_type=$4
+weight_decay=$5
+
+local_data_dir=/mnt/bd/xigua-data/fairseq_bin/xigua.en-zh.$mask.withtest.ed2.0
 
 
 fp16=1 #0
 lr=0.001
 warmup=4000
-max_tokens=512
+max_tokens=2048
 update_freq=1
 keep_last_epochs=10
 patience=10
 max_epoches=100
 dropout=0.3
-seed=1207
-weight_decay=0.1
 clip_norm=0.0
 residual_policy="None"
 ini_alpha=0.0
-arch=ve_double_cross_att_small
 
-
-video_feat_type="videoswin"
 if [ $video_feat_type == "clip" ]; then
         video_feat_dim=512
         visual_dir=/mnt/bd/kangliyan/data/vatex/images_tsv/
@@ -52,8 +52,8 @@ if [ $video_feat_type == "clip" ]; then
         videoswin_size=base
         kinetics=600
         video_feat_dim=1024
-        videoswin_path=/mnt/bd/kangliyan/models/swin_base_patch244_window877_kinetics600_22k.pth
-        visual_dir=/mnt/bd/kangliyan/data/vatex/images_tsv/
+        videoswin_path=/mnt/bd/xigua-slowfast-videoswin/models/swin_base_patch244_window877_kinetics600_22k.pth
+        visual_dir=/mnt/bd/xigua-data/tsv/
 fi
 max_num_frames=32
 img_res=224
@@ -62,11 +62,11 @@ freeze_backbone=True
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
 
-name=vatex_${mask}_arch${arch}_cri${cri}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_wd${weight_decay}_vtype${video_feat_type}_mf${max_num_frames}_rp${residual_policy}_ia${ini_alpha}
+name=${mask}ed20_arch${arch}_cri${cri}_tgt${tgt_lang}_lr${lr}_wu${warmup}_me${max_epoches}_seed${seed}_gpu${gpu_num}_wd${weight_decay}_vtype${video_feat_type}_mvlen${max_vid_len}_patience${patience}
 
-output_dir=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_output/vatex_0829/${mask}/ve_double_cross_att/${name}
-LOGS_DIR=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_logs/vatex_0829/${mask}/ve_double_cross_att
-local_logs_dir=~/fairseq_logs/vatex_0829/${mask}/ve_double_cross_att
+output_dir=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_output/xigua/${mask}/${name}
+LOGS_DIR=hdfs://haruna/home/byte_arnold_hl_mlnlc/user/kangliyan/fairseq_mmt/fairseq_logs/xigua/${mask}/
+local_logs_dir=~/fairseq_logs/xigua/${mask}/
 
 hdfs dfs -mkdir -p $LOGS_DIR
 hdfs dfs -mkdir -p $output_dir
@@ -78,16 +78,17 @@ hdfs dfs -put -f ${BASH_SOURCE[0]} $output_dir/train.sh
 
 fairseq-train $local_data_dir \
   --save-dir $output_dir \
-  --distributed-world-size $gpu_num -s $src_lang -t $tgt_lang \
+  --distributed-world-size $gpu_num -s $src_lang -t $tgt_lang --max-source-positions 256 --max-target-positions 256 \
   --arch $arch \
   --dropout $dropout \
   --weight-decay $weight_decay  \
   --clip-norm ${clip_norm}   \
-  --criterion $criterion --label-smoothing 0.1 \
+  --criterion $criterion --label-smoothing 0.1  \
   --task raw_video_translation \
   --optimizer adam --adam-betas '(0.9, 0.98)' \
   --lr $lr --min-lr 1e-09 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates $warmup \
-  --max-tokens $max_tokens --max-tokens-valid 256 --update-freq $update_freq  \
+  --max-tokens $max_tokens --max-tokens-valid 512 --update-freq $update_freq  \
+  --skip-invalid-size-inputs-valid-test \
   --seed $seed \
   --no-progress-bar  \
   --find-unused-parameters \
@@ -99,11 +100,12 @@ fairseq-train $local_data_dir \
   --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
   --patience $patience \
   --save-interval 1 \
+  --save-interval-updates 7 \
   --max-epoch ${max_epoches} --keep-interval-updates 10  --keep-best-checkpoints 10   \
   --visual-dir $visual_dir --video-feat-dim $video_feat_dim --img-res 224 --video-feat-type $video_feat_type   \
   --vidswin-size $videoswin_size --kinetics $kinetics --videoswin-path ${videoswin_path} --grid-feat --freeze-backbone \
   --residual-policy $residual_policy --ini-alpha $ini_alpha \
-  --num-workers 10 \
+  --num-workers 8 \
   --fp16  2>&1 | tee -a $local_logs_dir/log.${name}
 
 echo "---put log to $LOGS_DIR/log.${name}---"
