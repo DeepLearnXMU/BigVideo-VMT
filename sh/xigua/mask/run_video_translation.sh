cf2b7f4214c005033d073e6ab0395cd6e837dc26
kangliyan <kangliyan@bytedance.com>
2022-11-05 17:17:45 +0800

    1

--- sh/xigua/mask/run_video_translation.sh
+++ sh/xigua/mask/run_video_translation.sh
@@ -40,7 +40,7 @@ id_type=${13}
 
 
 if [ ${text_data} == "original" ]; then
-    local_data_dir=/mnt/bn/luyang/kangliyan/data/fairseq_bin/xigua.en-zh.annotations1016
+    local_data_dir=/mnt/bn/luyang/kangliyan/data/fairseq_bin/xigua.en-zh.annotations_1016
     elif [ ${text_data} == "asr" ]; then
       local_data_dir=~/data/fairseq_bin/xigua.en-zh.asr
     elif [ ${text_data} == "action_label_top5" ]; then
