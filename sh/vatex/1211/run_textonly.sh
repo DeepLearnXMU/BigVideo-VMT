052428e2001207d1c07da191c89c443c647366a0
kangliyan <kangliyan@bytedance.com>
2022-12-21 20:23:45 +0800

    1

--- sh/vatex/1211/run_textonly.sh
+++ sh/vatex/1211/run_textonly.sh
@@ -79,7 +79,7 @@ fairseq-train $local_data_dir \
   --eval-bleu-detok moses \
   --eval-bleu-remove-bpe \
   --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-  --max-epoch ${max_epoches} --keep-interval-updates 10 --keep-best-checkpoints 10  \
+  --max-epoch ${max_epoches} --keep-last-epochs 10 --keep-best-checkpoints 10  \
   --no-epoch-checkpoints  \
   --fp16  2>&1 | tee -a $local_logs_dir/log.${name}
 
