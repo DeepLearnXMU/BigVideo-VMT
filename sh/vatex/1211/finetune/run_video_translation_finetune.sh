65912baf4ab90b213e7e634fb1678efe80ae06ed
kangliyan <kangliyan@bytedance.com>
2023-01-04 14:55:45 +0800

    1

--- sh/vatex/1211/finetune/run_video_translation_finetune.sh
+++ sh/vatex/1211/finetune/run_video_translation_finetune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-device=0,1,2,3,4,5,6,7
+device=0
 export CUDA_VISIBLE_DEVICES=$device
 export http_proxy=http://bj-rd-proxy.byted.org:3128
 export https_proxy=http://bj-rd-proxy.byted.org:3128
