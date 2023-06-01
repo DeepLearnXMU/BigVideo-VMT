cfcd08c536e5da7e4a5faf6abb13ff9cf54b45ef
kangliyan <kangliyan@bytedance.com>
2023-06-01 20:19:08 +0800

    init_version

--- sh/vatex/binarize.sh
+++
@@ -1,9 +0,0 @@
-src='en'
-tgt='zh'
-TEXT='/home/sata/kly/videoNMT/data/preprocess_follow'
-fairseq-preprocess --source-lang $src --target-lang $tgt \
-  --trainpref $TEXT/train.char \
-  --validpref $TEXT/dev.char \
-  --testpref $TEXT/test.char \
-  --destdir $TEXT/data-bin/${src}_${tgt}.char \
-  --workers 8
